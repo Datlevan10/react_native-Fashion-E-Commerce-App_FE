5f8066f06c53a0cb68bde23b33c8ef819975b91b
Datvan10 <20050068@student.bdu.edu.vn>
2024-10-25 12:25:03 +0700

    move data type from string to date for Customer Admin and init Staff Order Order Detail

---
+++ database/migrations/2024_10_25_044156_update_date_of_birth_column_in_customers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE customers ALTER COLUMN date_of_birth TYPE date USING date_of_birth::date');
+        Schema::table('customers', function (Blueprint $table) {
+            $table->date('date_of_birth')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            //
+        });
+    }
+};
