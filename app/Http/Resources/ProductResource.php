b82385127d3fcc29f08f29406f9626d1e323a0c1
Datvan10 <20050068@student.bdu.edu.vn>
2024-10-29 03:06:58 +0700

    update API product

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -15,6 +15,24 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         // return parent::toArray($request);
+        // return [
+        //     'product_id' => $this->product_id,
+        //     'category_id' => $this->category_id,
+        //     'product_name' => $this->product_name,
+        //     'description' => $this->description,
+        //     'color' => $this->color,
+        //     'size' => $this->size,
+        //     'image' => $this->image,
+        //     'old_price' => $this->old_price,
+        //     'new_price' => $this->new_price,
+        //     // Handle data when returning json (490000.00 -> 490000)
+        //     // 'old_price' => rtrim(rtrim($this->old_price, '0'), '.'),
+        //     // 'new_price' => rtrim(rtrim($this->new_price, '0'), '.'),
+        //     'total_review' => $this->total_review,
+        //     'average_review' => $this->average_review,
+        //     'created_at' => $this->created_at,
+        //     'updated_at' => $this->updated_at,
+        // ];
         return [
             'product_id' => $this->product_id,
             'category_id' => $this->category_id,
@@ -25,9 +43,6 @@ class ProductResource extends JsonResource
             'image' => $this->image,
             'old_price' => $this->old_price,
             'new_price' => $this->new_price,
-            // Handle data when returning json (490000.00 -> 490000)
-            // 'old_price' => rtrim(rtrim($this->old_price, '0'), '.'),
-            // 'new_price' => rtrim(rtrim($this->new_price, '0'), '.'),
             'total_review' => $this->total_review,
             'average_review' => $this->average_review,
             'created_at' => $this->created_at,
