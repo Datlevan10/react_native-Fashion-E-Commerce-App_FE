43850913bf3639980efab9f20af8c32f34c6883b
Datvan10 <20050068@student.bdu.edu.vn>
2024-12-13 17:54:30 +0700

    fix method refreshAccessToken get new access_token and improve route api.php file

--- routes/api.php
+++ routes/api.php
@@ -23,20 +23,20 @@ Route::prefix('')->group(function () {
 
     // Event Routes
     Route::apiResource('events', EventController::class);
-    Route::get('events/is_active/active', [EventController::class, 'getActiveEvents']);
-    Route::get('events/is_active/inactive', [EventController::class, 'getInactiveEvents']);
-    Route::put('events/set-active/{event_id}', [EventController::class, 'activateEvent']);
-    Route::put('events/set-inactive/{event_id}', [EventController::class, 'deactivateEvent']);
+    Route::get('events/is-active/active', [EventController::class, 'getActiveEvents']);
+    Route::get('events/is-active/inactive', [EventController::class, 'getInactiveEvents']);
+    Route::put('events/{event_id}/set-active', [EventController::class, 'activateEvent']);
+    Route::put('events/{event_id}/set-inactive', [EventController::class, 'deactivateEvent']);
 
 
     // Category and Product Routes
     Route::apiResource('categories', CategoryController::class);
-    Route::get('/categories/limit/{limit}', [CategoryController::class, 'getLimitedCategories']);
+    Route::get('/categories/{limit}/limit', [CategoryController::class, 'getLimitedCategories']);
     Route::get('products/search', [ProductController::class, 'searchProducts']);
     Route::apiResource('products', ProductController::class);
     Route::get('products/category/{category_id}', [ProductController::class, 'getProductsByCategoryId']);
-    Route::get('products/category/{category_id}/limit/{limit}', [ProductController::class, 'getLimitedProductsByCategoryId']);
-    Route::get('/products/limit/{limit}', [ProductController::class, 'getLimitedProducts']);
+    Route::get('products/category/{category_id}/{limit}/limit', [ProductController::class, 'getLimitedProductsByCategoryId']);
+    Route::get('/products/{limit}/limit', [ProductController::class, 'getLimitedProducts']);
     Route::get('/products/search', [ProductController::class, 'searchProducts']);
 
     // User Management Routes (Admin, Staff, Customer)
@@ -45,7 +45,8 @@ Route::prefix('')->group(function () {
     Route::apiResource('customers', CustomerController::class);
     Route::middleware('api')->post('customers/auth/login', [CustomerController::class, 'authenticateLoginCustomer']);
     Route::middleware('auth:sanctum')->post('customers/auth/logout', [CustomerController::class, 'logout']);
-    Route::middleware('auth:sanctum')->post('customers/auth/refresh-token', [CustomerController::class, 'refreshAccessToken']);
+    Route::post('customers/auth/refresh-token', [CustomerController::class, 'refreshAccessToken']);
+    // Route::middleware('auth:sanctum')->post('customers/auth/refresh-token', [CustomerController::class, 'refreshAccessToken']);
     Route::middleware('auth:sanctum')->get('/profile', function (Request $request) {
         return response()->json($request->user());
     });
@@ -62,9 +63,9 @@ Route::prefix('')->group(function () {
 
     Route::resource('cart_details', CartDetailController::class)->except(['destroy']);
     Route::get('cart_details/cart/{cart_id}', [CartDetailController::class, 'getCartDetailByCartId']);
-    Route::get('cart_details/all-cart-details/customer/{customer_id}', [CartDetailController::class, 'getAllCartDetailByCustomerId']);
-    Route::get('cart_details/not-ordered-cart_details/customer/{customer_id}', [CartDetailController::class, 'getNotOrderedCartDetailByCustomerId']);
-    Route::delete('cart_details/delete-item-in-cart/{cart_detail_id}', [CartDetailController::class, 'deleteItemInCart']);
+    Route::get('cart_details/customer/{customer_id}/all-cart-details', [CartDetailController::class, 'getAllCartDetailByCustomerId']);
+    Route::get('cart_details/customer/{customer_id}/not-ordered-cart_details', [CartDetailController::class, 'getNotOrderedCartDetailByCustomerId']);
+    Route::delete('cart_details/{cart_detail_id}/delete-item-in-cart', [CartDetailController::class, 'deleteItemInCart']);
 
     // Order and Order Detail Routes
     Route::apiResource('orders', OrderController::class);
@@ -80,8 +81,8 @@ Route::prefix('')->group(function () {
     Route::get('/reviews/status/pending', [ReviewController::class, 'getPendingReviews']);
     Route::get('/reviews/status/approved', [ReviewController::class, 'getApprovedReviews']);
     Route::get('/reviews/status/unpublished', [ReviewController::class, 'getUnpublishedReviews']);
-    Route::put('/reviews/publish/{review_id}', [ReviewController::class, 'publishReview']);
-    Route::put('/reviews/unpublish/{review_id}', [ReviewController::class, 'unpublishReview']);
+    Route::put('/reviews/{review_id}/publish', [ReviewController::class, 'publishReview']);
+    Route::put('/reviews/{review_id}/unpublish', [ReviewController::class, 'unpublishReview']);
 
     // Authenticated User Route
     Route::get('/user', function (Request $request) {
