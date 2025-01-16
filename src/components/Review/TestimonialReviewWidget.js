6a1f43f055f0034513bce623469072ecc84a6e79
Datvan10 <20050068@student.bdu.edu.vn>
2025-01-17 02:10:34 +0700

    improve feature Testimonial review widget

--- src/components/Review/TestimonialReviewWidget.js
+++ src/components/Review/TestimonialReviewWidget.js
@@ -6,7 +6,7 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
-import { Ionicons, AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Entypo, AntDesign, FontAwesome } from "@expo/vector-icons";
 import Colors from "../../styles/Color";
 
 const TestimonialReviewWidget = ({ reviews }) => {
@@ -25,28 +25,45 @@ const TestimonialReviewWidget = ({ reviews }) => {
         }
         return nextIndex;
       });
-    }, 3000); // Thay đổi mỗi 3 giây
+    }, 3000);
 
-    return () => clearInterval(interval); // Dọn dẹp interval khi component unmount
+    return () => clearInterval(interval);
   }, [reviews.length]);
 
   const previousReview = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? reviews.length - 1 : prevIndex - 1
-    );
+    setCurrentIndex((prevIndex) => {
+      const newIndex = prevIndex === 0 ? reviews.length - 1 : prevIndex - 1;
+      if (flatListRef.current) {
+        flatListRef.current.scrollToIndex({
+          animated: true,
+          index: newIndex,
+        });
+      }
+      return newIndex;
+    });
   };
 
   const nextReview = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === reviews.length - 1 ? 0 : prevIndex + 1
-    );
+    setCurrentIndex((prevIndex) => {
+      const newIndex = prevIndex === reviews.length - 1 ? 0 : prevIndex + 1;
+      if (flatListRef.current) {
+        flatListRef.current.scrollToIndex({
+          animated: true,
+          index: newIndex,
+        });
+      }
+      return newIndex;
+    });
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Our Customers Love Us</Text>
       <View style={styles.reviewContainer}>
-        <TouchableOpacity onPress={previousReview} style={styles.navigation}>
+        <TouchableOpacity
+          onPress={previousReview}
+          style={styles.navigationLeft}
+        >
           <AntDesign name="left" size={18} color={Colors.whiteBgColor} />
         </TouchableOpacity>
         <FlatList
@@ -56,6 +73,7 @@ const TestimonialReviewWidget = ({ reviews }) => {
           horizontal
           renderItem={({ item }) => (
             <View style={styles.reviewBox}>
+              <Entypo name="quote" size={24} color={Colors.darkGray} />
               <Text style={styles.reviewContent}>{item.review_product}</Text>
               <Text style={styles.customerName}>{item.customer_name}</Text>
               <View style={styles.rating}>
@@ -63,23 +81,20 @@ const TestimonialReviewWidget = ({ reviews }) => {
                   <FontAwesome
                     key={index}
                     name="star"
-                    size={20}
+                    size={16}
                     color={Colors.yellowColor}
                   />
                 ))}
               </View>
-              {/* <Text style={styles.reviewDate}>
-                {new Date(item.review_date).toLocaleDateString()}
-              </Text> */}
             </View>
           )}
           showsHorizontalScrollIndicator={false}
-          snapToInterval={220}
-          snapToAlignment="start"
+          snapToAlignment="center"
           decelerationRate="fast"
           onScrollToIndexFailed={() => {}}
         />
-        <TouchableOpacity onPress={nextReview} style={styles.navigation}>
+
+        <TouchableOpacity onPress={nextReview} style={styles.navigationRight}>
           <AntDesign name="right" size={16} color={Colors.whiteBgColor} />
         </TouchableOpacity>
       </View>
@@ -109,37 +124,52 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     paddingHorizontal: 10,
     backgroundColor: "#f9f9f9",
+    justifyContent: "space-between",
     borderRadius: 10,
     alignItems: "center",
-    // marginRight: 10,
-    width: 170,
-    marginHorizontal: 5,
+    marginRight: 10,
+    width: 180,
+    height: 200,
+    // marginHorizontal: 5,
   },
   customerName: {
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: "400",
     marginTop: 30,
   },
   reviewContent: {
     fontSize: 16,
     textAlign: "center",
+    marginTop: 15,
   },
   rating: {
     flexDirection: "row",
     marginTop: 5,
+    gap: 3,
   },
   reviewDate: {
     fontSize: 12,
     color: "#888",
     marginTop: 5,
   },
-  navigation: {
+  navigationLeft: {
     backgroundColor: Colors.darkGray,
     width: 20,
     height: 40,
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 5,
-    marginHorizontal: 0,
+    // marginRight: 5,
+    left: -7,
+  },
+  navigationRight: {
+    backgroundColor: Colors.darkGray,
+    width: 20,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    // marginLeft: 5,
+    right: -7,
   },
 });
